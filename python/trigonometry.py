48a958aa7e8409444a64d690c63eec24f590ec59
Paul Novaes <n3times@users.noreply.github.com>
2020-02-05 19:21:44 -0800

    Refactor trigonometry functions.

--- python/trigonometry.py
+++ python/trigonometry.py
@@ -8,132 +8,82 @@ class TrigMode:
     RAD = 2
     GRAD = 3
 
-def convert_to_radians(n, mode):
-    if abs(n.exp) >= 10:
-        return OpResult(n, OpError.DATA_ERROR)
-    x = n_to_x(n)
-    if mode == TrigMode.GRAD:
-        x = x / 400 * 2 * math.pi
-    if mode == TrigMode.DEG:
-        x = x / 360 * 2 * math.pi
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
-
-def convert_from_radians(x, mode):
-    if mode == TrigMode.RAD:
-        return x
-    elif mode == TrigMode.GRAD:
-        return x / 2 / math.pi * 400
-    else:
-        return x / 2 / math.pi * 360
-
-    if abs(n.exp) >= 10:
-        return OpResult(n, OpError.DATA_ERROR)
-    x = n_to_x(n)
-    if mode == TrigMode.GRAD:
-        x = x / 400 * 2 * math.pi
-    if mode == TrigMode.DEG:
-        x = x / 360 * 2 * math.pi
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
- 
 def cos(n, mode):
-    result = convert_to_radians(n, mode)
-    if result.error != OpError.OK:
-        return OpResult(n, result.error)
-
-    x = n_to_x(result.n)
-    x = math.cos(x)
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
+    return trig(math.cos, n, mode)
  
 def sin(n, mode):
-    result = convert_to_radians(n, mode)
-    if result.error != OpError.OK:
-        return OpResult(n, result.error)
-
-    x = n_to_x(result.n)
-    x = math.sin(x)
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
+    return trig(math.sin, n, mode)
 
 def tan(n, mode):
-    result = convert_to_radians(n, mode)
-    if result.error != OpError.OK:
-        return OpResult(n, result.error)
-
-    x = n_to_x(result.n)
-    x = math.tan(x)
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
+    return trig(math.tan, n, mode)
 
 def arcsin(n, mode):
-    x = n_to_x(n)
-    if abs(x) > 1:
-        return OpResult(n, OpError.DATA_ERROR)
-
-    x = math.asin(x)
-    x = convert_from_radians(x, mode)
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
+    return arctrig(math.asin, n, mode)
 
 def arccos(n, mode):
-    x = n_to_x(n)
-    if abs(x) > 1:
-        return OpResult(n, OpError.DATA_ERROR)
-
-    x = math.acos(x)
-    x = convert_from_radians(x, mode)
-
-    n = x_to_n(x)
-    return OpResult(n, OpError.OK)
+    return arctrig(math.acos, n, mode)
 
 def arctan(n, mode):
-    x = n_to_x(n)
+    return arctrig(math.atan, n, mode)
 
-    x = math.atan(x)
-    x = convert_from_radians(x, mode)
+"""
+
+Utility functions.
+
+"""
+
+def trig(f, n, mode):
+    x = n_to_x(n)
+    x = reduce_angle(x, mode)
+    x = convert_to_radians(x, mode)
+
+    x = f(x)
 
     n = x_to_n(x)
     return OpResult(n, OpError.OK)
 
+def arctrig(f, n, mode):
+    x = n_to_x(n)
+    if f == math.asin or f == math.acos:
+        if abs(x) > 1:
+            return OpResult(n, OpError.DATA_ERROR)
+
+    x = f(x)
+
+    x = convert_from_radians(x, mode)
+    n = x_to_n(x)
+    return OpResult(n, OpError.OK)
+
+def reduce_angle(x, mode):
+    if mode == TrigMode.RAD:
+        x = x % (2 * math.pi)
+    elif mode == TrigMode.DEG:
+        x = x % 360
+    elif mode == TrigMode.GRAD:
+        x = x % 400
+    return x
+
+def convert_to_radians(x, mode):
+    if mode == TrigMode.GRAD:
+        x = 2 * math.pi * (x / 400)
+    elif mode == TrigMode.DEG:
+        x = 2 * math.pi * (x / 360)
+    return x
+
+def convert_from_radians(x, mode):
+    if mode == TrigMode.GRAD:
+        x = x / 2 / math.pi * 400
+    elif mode == TrigMode.DEG:
+        x = x / 2 / math.pi * 360
+    return x
 """
 
-Direct Tests.
+Tests.
 
 """
-
-def approximately_equal(actual, expected):
-    if abs(actual) > 1e10 and abs(expected) > 1e10:
-        return True
-    if abs(actual) < 1e-11 and abs(expected) < 1e-11:
-        return True
-    if abs(actual) >= 1e-11 and abs(expected) < 1e-11:
-        return False
-    if abs(actual) < 1e-11 and abs(expected) >= 1e-11:
-        return False
-    if abs(1 - actual/expected) < 1e-11:
-        return True
-    return False
-
-def test_base(cases, expect, mode, f):
-    n = len(cases)
-    assert(n == len(expect))
-    for i in range(n):
-        n = x_to_n(cases[i])
-        result = f(n, mode) 
-        actual = n_to_x(result.n)
-        expected = expect[i]
-        print(actual, expected)
-        assert(approximately_equal(actual, expected))
-
 S_2_2 = math.sqrt(2) / 2
 INF = 1e99
+PI = math.pi
 
 RAD_CASES = [i * math.pi / 4 for i in range(9)]
 DEG_CASES = [i * 45 for i in range(9)]
@@ -142,24 +92,6 @@ GRAD_CASES = [i * 50 for i in range(9)]
 SIN_EXPECTED = [0, S_2_2, 1, S_2_2, 0, -S_2_2, -1, -S_2_2, 0]
 COS_EXPECTED = [1, S_2_2, 0, -S_2_2, -1, -S_2_2, 0, S_2_2, 1]
 TAN_EXPECTED = [0, 1, INF, -1, 0, 1, -INF, -1, 0]
-
-def test_all_modes(f, expected):
-    test_base(RAD_CASES, expected, TrigMode.RAD, f)
-    test_base(DEG_CASES, expected, TrigMode.DEG, f)
-    test_base(GRAD_CASES, expected, TrigMode.GRAD, f)
-
-def test_direct():
-    test_all_modes(cos, COS_EXPECTED)
-    test_all_modes(sin, SIN_EXPECTED)
-    test_all_modes(tan, TAN_EXPECTED)
-
-"""
-
-Arc Tests.
-
-"""
-
-PI = math.pi
 
 ARC_SIN_CASES = [-1, -S_2_2, 0, S_2_2, 1]
 ARC_COS_CASES = [-1, -S_2_2, 0, S_2_2, 1]
@@ -175,15 +107,51 @@ ARC_TAN_RAD_EXPECTED = [-PI/2, -PI/4, 0, PI/4, PI/2]
 ARC_TAN_DEG_EXPECTED = [-90, -45, 0, 45, 90]
 ARC_TAN_GRAD_EXPECTED = [-100, -50, 0, 50, 100]
 
-def test_arc():
-    test_base(ARC_COS_CASES, ARC_COS_RAD_EXPECTED, TrigMode.RAD, arccos)
-    test_base(ARC_COS_CASES, ARC_COS_DEG_EXPECTED, TrigMode.DEG, arccos)
-    test_base(ARC_COS_CASES, ARC_COS_GRAD_EXPECTED, TrigMode.GRAD, arccos)
-    test_base(ARC_SIN_CASES, ARC_SIN_RAD_EXPECTED, TrigMode.RAD, arcsin)
-    test_base(ARC_SIN_CASES, ARC_SIN_DEG_EXPECTED, TrigMode.DEG, arcsin)
-    test_base(ARC_SIN_CASES, ARC_SIN_GRAD_EXPECTED, TrigMode.GRAD, arcsin)
-    test_base(ARC_TAN_CASES, ARC_TAN_RAD_EXPECTED, TrigMode.RAD, arctan)
-    test_base(ARC_TAN_CASES, ARC_TAN_DEG_EXPECTED, TrigMode.DEG, arctan)
-    test_base(ARC_TAN_CASES, ARC_TAN_GRAD_EXPECTED, TrigMode.GRAD, arctan)
+def approximately_equal(actual, expected):
+    if abs(actual) > 1e10 and abs(expected) > 1e10:
+        return True
+    if abs(actual) < 1e-11 and abs(expected) < 1e-11:
+        return True
+    if abs(actual) >= 1e-11 and abs(expected) < 1e-11:
+        return False
+    if abs(actual) < 1e-11 and abs(expected) >= 1e-11:
+        return False
+    if abs(1 - actual/expected) < 1e-11:
+        return True
+    return False
 
-test_arc()
+def test_base(f, mode, cases, expect):
+    assert(len(cases) == len(expect))
+    n = len(cases)
+    for i in range(n):
+        n = x_to_n(cases[i])
+        result = f(n, mode) 
+        actual = n_to_x(result.n)
+        expected = expect[i]
+        print(actual, expected)
+        assert(approximately_equal(actual, expected))
+
+def test_cases():
+    # Direct.
+    test_base(cos, TrigMode.RAD, RAD_CASES, COS_EXPECTED)
+    test_base(cos, TrigMode.DEG, DEG_CASES, COS_EXPECTED)
+    test_base(cos, TrigMode.GRAD, GRAD_CASES, COS_EXPECTED)
+    test_base(sin, TrigMode.RAD, RAD_CASES, SIN_EXPECTED)
+    test_base(sin, TrigMode.DEG, DEG_CASES, SIN_EXPECTED)
+    test_base(sin, TrigMode.GRAD, GRAD_CASES, SIN_EXPECTED)
+    test_base(tan, TrigMode.RAD, RAD_CASES, TAN_EXPECTED)
+    test_base(tan, TrigMode.DEG, DEG_CASES, TAN_EXPECTED)
+    test_base(tan, TrigMode.GRAD, GRAD_CASES, TAN_EXPECTED)
+
+    # Arc.
+    test_base(arccos, TrigMode.RAD, ARC_COS_CASES, ARC_COS_RAD_EXPECTED)
+    test_base(arccos, TrigMode.DEG, ARC_COS_CASES, ARC_COS_DEG_EXPECTED)
+    test_base(arccos, TrigMode.GRAD, ARC_COS_CASES, ARC_COS_GRAD_EXPECTED)
+    test_base(arcsin, TrigMode.RAD, ARC_SIN_CASES, ARC_SIN_RAD_EXPECTED)
+    test_base(arcsin, TrigMode.DEG, ARC_SIN_CASES, ARC_SIN_DEG_EXPECTED)
+    test_base(arcsin, TrigMode.GRAD, ARC_SIN_CASES, ARC_SIN_GRAD_EXPECTED)
+    test_base(arctan, TrigMode.RAD, ARC_TAN_CASES, ARC_TAN_RAD_EXPECTED)
+    test_base(arctan, TrigMode.DEG, ARC_TAN_CASES, ARC_TAN_DEG_EXPECTED)
+    test_base(arctan, TrigMode.GRAD, ARC_TAN_CASES, ARC_TAN_GRAD_EXPECTED)
+
+test_cases()
