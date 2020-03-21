5c19ff58b44fed6d352b2f37f506be30cabdf7cd
n3times <n3times@users.noreply.github.com>
2020-03-20 18:31:03 -0700

    Allow Python to use engine.

--- Makefile
+++ Makefile
@@ -2,12 +2,10 @@ all:
 	make -C cli
 	make -C mini
 	make -C common/tests
-
-lib:
-	make lib -C cli
-	make lib -C mini
+	make python
 
 clean:
 	make clean -C cli
 	make clean -C mini
 	make clean -C common/tests
+	make clean -C python
---
+++ python/penta1000.py
@@ -0,0 +1,53 @@
+import ctypes
+
+from ctypes.util import find_library
+
+def p1_new(random_seed):
+    global libp1
+    libp1 = ctypes.cdll.LoadLibrary(find_library('libpenta1000.so'))
+    # core
+    libp1.p1_new.argtypes = [ctypes.c_long]
+    libp1.p1_new.restype = ctypes.c_void_p
+    libp1.p1_release.argtypes = [ctypes.c_void_p]
+    libp1.p1_press_key.argtypes = [ctypes.c_void_p, ctypes.c_char]
+    libp1.p1_get_display.argtypes = [ctypes.c_void_p]
+    libp1.p1_get_display.restype = ctypes.c_char_p
+    # animations
+    libp1.p1_advance_frame.argtypes = [ctypes.c_void_p]
+    libp1.p1_is_animating.argtypes = [ctypes.c_void_p]
+    libp1.p1_is_animating.restype = ctypes.c_bool
+    # serialization
+    libp1.p1_serialize.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
+    libp1.p1_serialize.restype = ctypes.c_void_p
+    libp1.p1_deserialize.argtypes = [ctypes.c_void_p]
+    libp1.p1_deserialize.restype = ctypes.c_void_p
+    # logging
+    libp1.p1_log_get_first_available_index.argtypes = [ctypes.c_void_p]
+    libp1.p1_log_get_first_available_index.restype = ctypes.c_long
+    libp1.p1_log_get_last_available_index.argtypes = [ctypes.c_void_p]
+    libp1.p1_log_get_last_available_index.restype = ctypes.c_long
+    libp1.p1_log_get_entry.argtypes = [ctypes.c_void_p, ctypes.c_long]
+    libp1.p1_log_get_entry.restype = ctypes.c_char_p
+    libp1.p1_log_clear.argtypes = [ctypes.c_void_p]
+    # return
+    return libp1.p1_new(random_seed)
+
+def p1_press_key(p1, key):
+    return libp1.p1_press_key(p1, ord(key))
+
+def p1_get_display(p1):
+    return libp1.p1_get_display(p1).decode("ascii")
+
+def test():
+    p1 = p1_new(0)
+    print(p1_get_display(p1))
+    p1_press_key(p1,'1')
+    p1_press_key(p1,'+')
+    p1_press_key(p1,'1')
+    p1_press_key(p1,'=')
+    p1_press_key(p1,'/')
+    p1_press_key(p1,'0')
+    p1_press_key(p1,'=')
+    print(p1_get_display(p1))
+
+test()
