5c841e786b7af2ff826c145584c4e7676b707210
n3times <n3times@users.noreply.github.com>
2020-03-21 21:17:36 -0700

    Tweak Python app.

--- python/simple.py
+++ python/simple.py
@@ -6,9 +6,9 @@ from tkinter import *
 class App(Frame):
     def __init__(self):
         Frame.__init__(self)
-        self.pack(expand=YES, fill=BOTH)
         self.master.title("Pentatronics 1000")
         self['bg'] = 'black'
+        self.pack(expand=YES, fill=BOTH)
 
         # Place display at the top.
         display = StringVar()
@@ -20,21 +20,21 @@ class App(Frame):
         # Set display to initial value.
         display.set(p1_get_display(p1))
 
-        # Below the display, place the 20 caclculator buttons in a grid.
-        keys = ["g~%c", "789/", "456*", "123-", "0.=+"]
+        # Below the display, place the 20 calculator buttons in a grid.
+        keys = ["?~%C", "789/", "456*", "123-", "0.=+"]
         for row in range(5):
             for col in range(4):
                 key = keys[row][col]
                 button = Button(self, text=key, font='courier 30 bold')
-                bg = 'white'
-                if key == 'c': bg = 'yellow'
-                if key == 'g': bg = 'gray'
-                button['highlightbackground'] = bg
-                button['command'] = lambda d=display, k=key: self.calc(d, k)
+                button['command'] = lambda d=display, k=key:self.press_key(d, k)
                 button.grid(row=row+1, column=col, sticky='ew', padx=2, pady=2,
                             ipady=10)
 
-    def calc(self, display, key):
+    # Called whenever the user presses a calculator key.
+    def press_key(self, display, key):
+        # Translate displayed key to key understandable by the engine.
+        if key == '?': key = 'g'
+        if key == 'C': key = 'c'
         # Report key press to engine.
         p1_press_key(p1, key)
         # Ignore animations.
@@ -45,6 +45,6 @@ class App(Frame):
         display.set(p1_get_display(p1))
 
 if __name__ == '__main__':
-    # Initialize the engine (p1) that emulates a Pentatronics 1000.
+    # Initialize the engine that drives the emulation of the Pentatronics 1000.
     p1 = p1_new(0)
     App().mainloop()
