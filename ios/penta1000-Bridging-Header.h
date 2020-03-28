b221a6c0c8b9d12198c6c546a5ff32ac73850cf2
n3times <n3times@users.noreply.github.com>
2020-03-27 17:21:23 -0700

    Create iOS project.

---
+++ ios/penta1000-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  penta1000-Bridging-Header.h
+//  penta1000
+//
+//  Created by Paul Novaes on 3/27/20.
+//  Copyright © 2020 Paul Novaes. All rights reserved.
+//
+
+#ifndef penta1000_Bridging_Header_h
+#define penta1000_Bridging_Header_h
+
+#include "../engine/include/penta1000.h"
+
+#endif /* penta1000_Bridging_Header_h */
---
+++ ios/penta1000Tests/penta1000Tests.swift
@@ -0,0 +1,34 @@
+//
+//  penta1000Tests.swift
+//  penta1000Tests
+//
+//  Created by Paul Novaes on 3/27/20.
+//  Copyright © 2020 Paul Novaes. All rights reserved.
+//
+
+import XCTest
+@testable import penta1000
+
+class penta1000Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
