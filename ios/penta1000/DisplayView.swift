5a28040e70e9e973fb886125e271695f805d3a32
n3times <n3times@users.noreply.github.com>
2020-04-03 20:19:16 -0700

    [ios] Improve display L&F.

--- ios/penta1000/DisplayView.swift
+++ ios/penta1000/DisplayView.swift
@@ -7,7 +7,7 @@ struct DisplayView: View {
 
     let charToSegments10 = [
         "0": 0b1101001011,
-        "1": 0b0001000010,
+        "1": 0b0010000100,
         "2": 0b1001111001,
         "3": 0b1001110011,
         "4": 0b0101110010,
@@ -21,22 +21,23 @@ struct DisplayView: View {
         "C": 0b1100001001,
         "D": 0b0001111011,
         "E": 0b1100111001,
+        "e": 0b0000101101,
         "F": 0b1100111000,
         "G": 0b1100001011,
         "H": 0b0101111010,
-        "I": 0b0001000010,
+        "I": 0b1010000101,
         "J": 0b0001000011,
         "L": 0b0100001001,
-        "N": 0b0000111010,
+        "N": 0b1101001010,
         "O": 0b1101001011,
         "P": 0b1101111000,
-        "R": 0b0000111000,
+        "R": 0b1101111100,
         "S": 0b1100110011,
-        "T": 0b0100111000,
+        "T": 0b1010000100,
         "U": 0b0101001011,
         "V": 0b0101001011,
         "W": 0b1000001011,
-        "Y": 0b0101110010,
+        "Y": 0b0101110100,
         "Z": 0b1001111001,
         "_": 0b0000000001,
         "-": 0b0000110000,
@@ -48,18 +49,18 @@ struct DisplayView: View {
 
     let segmentRects10 = [
         0: CGRect(x: 2.0, y: 0, width: 16, height: 4),
-        1: CGRect(x: 0.0, y: 2, width: 4, height: 16),
-        2: CGRect(x: 8.0, y: 4, width: 4, height: 14),
-        3: CGRect(x: 16.0, y: 2, width: 4, height: 16),
-        4: CGRect(x: 2.0, y: 16, width: 8, height: 4),
-        5: CGRect(x: 10.0, y: 16, width: 8, height: 4),
-        6: CGRect(x: 0.0, y: 18, width: 4, height: 16),
-        7: CGRect(x: 8.0, y: 18, width: 4, height: 14),
-        8: CGRect(x: 16.0, y: 18, width: 4, height: 16),
-        9: CGRect(x: 2.0, y: 32, width: 16, height: 4),
+        1: CGRect(x: 0.0, y: 2, width: 4, height: 14),
+        2: CGRect(x: 8.0, y: 2, width: 4, height: 14),
+        3: CGRect(x: 16.0, y: 2, width: 4, height: 14),
+        4: CGRect(x: 2.0, y: 14, width: 8, height: 4),
+        5: CGRect(x: 10.0, y: 14, width: 8, height: 4),
+        6: CGRect(x: 0.0, y: 16, width: 4, height: 14),
+        7: CGRect(x: 8.0, y: 16, width: 4, height: 14),
+        8: CGRect(x: 16.0, y: 16, width: 4, height: 14),
+        9: CGRect(x: 2.0, y: 28, width: 16, height: 4),
     ]
 
-    let dotData = CGRect(x: 22, y: 31, width: 5, height: 5)
+    let dotData = CGRect(x: 22, y: 27, width: 5, height: 5)
 
     init(_ displayText: Binding<String>) {
         self._displayText = displayText
@@ -112,7 +113,7 @@ struct DisplayView: View {
                 if c == "." { continue }
                 let hasDot = i < 15 && characters[i + 1] == "."
                 let startX = 30 + 30 * Double(index)
-                let startY = 225.0
+                let startY = 229.0
                 let ledPath =
                     getLedPath(c: c, startX: startX, startY: startY, hasDot: hasDot)
                 if ledPath != nil { path.addPath(ledPath!) }
