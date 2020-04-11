b3c42cacc25785931824cd92e18ada13b21aa541
n3times <n3times@users.noreply.github.com>
2020-04-11 15:41:54 -0700

    [ios] Clean up main view.

--- ios/penta1000/Penta1000View.swift
+++ ios/penta1000/Penta1000View.swift
@@ -2,34 +2,32 @@ import SwiftUI
 
 struct Penta1000View: View {
     private let penta1000: Penta1000
-    @State var displayText: String = "READY"
-    @State var isTapped = false
+    @State private var displayText: String = "READY"
+    @State private var isTapped = false
 
     init(penta1000: Penta1000) {
         self.penta1000 = penta1000
     }
 
     // Returns the key at a given location, or nil if there is no such a key.
-    private func getCalculatorKey(location: CGPoint, calcWidth: CGFloat, calcHeight: CGFloat) -> Character? {
-        let factor = calcWidth / 375
-        let x = location.x / factor
-        let y = location.y / factor
-
-
+    private func getCalculatorKey(standardizedLocation: CGPoint) -> Character? {
         // Top left corner of top left key ("?").
-        let x0: CGFloat = 37
-        let y0: CGFloat = 313
+        let x0 = 37.0
+        let y0 = 313.0
 
         // Separation between the top left corners of consecutive keys.
-        let dx: CGFloat = 78.5
-        let dy: CGFloat = 67
+        let dx = 78.5
+        let dy = 67.0
 
         // Dimensions of each key.
-        let w: CGFloat = 65
-        let h: CGFloat = 51
+        let w = 65.0
+        let h = 51.0
 
-        var i: CGFloat = (x - x0 + (dx - w)/2) / dx
-        var j: CGFloat = (y - y0 + (dy - h)/2) / dy
+        let x = Double(standardizedLocation.x) - x0
+        let y = Double(standardizedLocation.y) - y0
+
+        var i = (x + (dx - w)/2) / dx
+        var j = (y + (dy - h)/2) / dy
 
         if (i >= -0.2 && j >= -0.2 && i < 4.2 && j < 5.2) {
             if i < 0 { i = 0 }
@@ -47,8 +45,9 @@ struct Penta1000View: View {
     private func appeared() {
         displayText = penta1000.display()
         if self.penta1000.isAnimating() {
-            Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { timer in
+            Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true, block: { timer in
                 self.penta1000.advanceFrame()
+                if (self.penta1000.isAnimating()) { self.penta1000.advanceFrame() }
                 self.displayText = self.penta1000.display()
 
                 if !self.penta1000.isAnimating() {
@@ -59,67 +58,76 @@ struct Penta1000View: View {
     }
 
     func getView(_ metrics: GeometryProxy) -> some View {
-        let screenWidth = metrics.size.width
-        let screenHeight = metrics.size.height
+        let standardCalcWidth = 375.0
+        let standardCalcHeight = 647.0
+        let standardDisplayHeight = 96.0
+        let standardDisplayOffsetY = 197.0
+
+        let screenWidth = Double(metrics.size.width)
+        let screenHeight = Double(metrics.size.height)
+
         let screenAspectRatio = screenHeight / screenWidth
+        let calcAspectRatio = standardCalcHeight / standardCalcWidth
+        let isPortrait = screenAspectRatio >= calcAspectRatio
 
-        let standardizedWidth = CGFloat(375.0)
-        let standardizedHeight = CGFloat(647.0)
-        let standardizedScreenAspectRatio = standardizedHeight / standardizedWidth
+        let calcWidth = isPortrait ? screenWidth : screenHeight / calcAspectRatio
+        let calcHeight = isPortrait ? screenWidth * calcAspectRatio : screenHeight
 
-        let isPortrait = screenAspectRatio >= CGFloat(standardizedScreenAspectRatio)
-
-        let calcWidth = isPortrait ? screenWidth : screenHeight / standardizedScreenAspectRatio
-        let calcHeight = isPortrait ? screenWidth * standardizedScreenAspectRatio : screenHeight
-
-        // Display is full width, standard height of 96 and standard offset_y of 197
+        let factor = calcWidth / standardCalcWidth
 
         let displayWidth = calcWidth
-        let displayHeight = CGFloat(96.0) * calcHeight / standardizedHeight
+        let displayHeight = standardDisplayHeight * factor
 
         let displayOffsetX = CGFloat(0.0)
-
-        let displayOffsetY = (197 - (647 - 96)/2) * calcHeight / standardizedHeight
+        let displayOffsetY =
+            (standardDisplayOffsetY - (standardCalcHeight - standardDisplayHeight)/2) * factor
 
         return ZStack {
             Color(red: 16/255, green: 16/255, blue: 16/255).edgesIgnoringSafeArea(.all)
             Image("penta1000")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: calcWidth, height: calcHeight, alignment: .center)
+                .frame(width: CGFloat(calcWidth), height: CGFloat(calcHeight), alignment: .center)
                 .onAppear(perform: self.appeared)
                 .gesture(
                     DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                    .onChanged {
-                        if (self.isTapped) { return; }
-                        self.isTapped = true;
-                        let c = self.getCalculatorKey(
-                            location: $0.location, calcWidth: calcWidth, calcHeight: calcHeight)
-                        if c != nil {
-                            let wasAnimating = self.penta1000.isAnimating()
-                            self.penta1000.pressKey(c: c!)
-                            let isAnimating = self.penta1000.isAnimating()
-                            self.displayText = self.penta1000.display()
-                            if !wasAnimating && isAnimating {
-                                Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
-                                    self.penta1000.advanceFrame()
-                                    self.displayText = self.penta1000.display()
+                        .onChanged {
+                            if (self.isTapped) { return; }
+                            self.isTapped = true;
+                            let standardizedLocation = CGPoint(x: $0.location.x / CGFloat(factor),
+                                                               y: $0.location.y / CGFloat(factor))
+                            let c =
+                                self.getCalculatorKey(standardizedLocation: standardizedLocation)
+                            if c != nil {
+                                let wasAnimating = self.penta1000.isAnimating()
+                                self.penta1000.pressKey(c: c!)
+                                let isAnimating = self.penta1000.isAnimating()
+                                self.displayText = self.penta1000.display()
+                                if !wasAnimating && isAnimating {
+                                    Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { timer in
+                                        self.penta1000.advanceFrame()
+                                        if (self.penta1000.isAnimating()) {
+                                            self.penta1000.advanceFrame()
+                                        }
+                                        self.displayText = self.penta1000.display()
 
-                                    if !self.penta1000.isAnimating() {
-                                        timer.invalidate()
+                                        if !self.penta1000.isAnimating() {
+                                            timer.invalidate()
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                    .onEnded { _ in
-                        self.isTapped = false
-                    })
+                        .onEnded { _ in
+                            self.isTapped = false
+                        }
+                )
                 .accessibility(identifier: "calculator")
                 .accessibility(label: Text(self.displayText))
             DisplayView(self.$displayText)
-                .offset(x: displayOffsetX, y: displayOffsetY)
-                .frame(width: displayWidth, height: displayHeight, alignment: .center)
+                .offset(x: CGFloat(displayOffsetX), y: CGFloat(displayOffsetY))
+                .frame(width: CGFloat(displayWidth),
+                       height: CGFloat(displayHeight),
+                       alignment: .center)
         }
     }
 
